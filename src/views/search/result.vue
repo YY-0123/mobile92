434249712a4de23a0ba9e2f7840980bd3bbcda92
YY-0123 <2579439253@qq.com>
2020-02-20 22:21:38 +0800

    文章搜索-搜索结果-页面布局-获取展示结果

--- src/views/search/result.vue
+++ src/views/search/result.vue
@@ -1,13 +1,97 @@
 <template>
   <div class="container">
-    <van-nav-bar title="搜索结果" left-arrow @click-left="$router.back()" />
+    <!--给导航栏设置fixed，使得固定定位-->
+    <van-nav-bar fixed title="搜索结果" left-arrow @click-left="$router.back()" />
+    <!-- 瀑布流加载 -->
+    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+      <van-cell v-for="item in searchList" :key="item.art_id.toString()" :title="item.title" />
+    </van-list>
   </div>
 </template>
 
 <script>
+// 引入api接口
+import { apiSearchList } from '@/api/search';
 export default {
-  name: "search-result"
-};
+  name: 'search-result',
+  computed: {
+    // 获得路由参数q，使得组件内部可以通过 this.q 的简便方式获得路由参数
+    q () {
+      return this.$route.params.q
+    }
+  },
+  data () {
+    return {
+      // 瀑布相关
+      list: [],
+      loading: false,
+      finished: false,
+      searchList: [], // 搜索结果
+      page: 1, // 页码
+      per_page: 10 // 条数
+    }
+  },
+  // created () {
+  //   // 调用搜索方法
+  //   this.getSearchList()
+  // },
+  methods: {
+    // 瀑布加载
+    async onLoad () {
+      await this.$sleep(1000) // 暂停1s，不要太着急加载
+
+      let result = await apiSearchList({
+        q: this.q,
+        page: this.page,
+        per_page: this.per_page
+      })
+      // 加载状态结束
+      this.loading = false
+
+      if (!result.results.length) {
+        // 数据全部加载完成
+        this.finished = true
+        return false
+      }
+      // 追加数据
+      this.searchList.push(...result.results)
+      // 页码累加
+      this.page++
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      // setTimeout(() => {
+      //   for (let i = 0; i < 10; i++) {
+      //     this.list.push(this.list.length + 1)
+      //   }
+
+      //   // 加载状态结束
+      //   this.loading = false
+
+      //   // 数据全部加载完成
+      //   if (this.list.length >= 40) {
+      //     this.finished = true
+      //   }
+      // }, 1000)
+    }
+    // 获得搜索结果
+    // async getSearchList () {
+    //   const result = await apiSearchList({ q: this.q })
+    //   this.searchList = result.results
+    // }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style lang="less" scoped>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  box-sizing: border-box;
+  .van-list {
+    flex: 1;
+    margin-top: 92px;
+  }
+}
+</style>
