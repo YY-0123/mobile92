412f0d5edf8c663ba804fb38e81312fc6325c037
YY-0123 <2579439253@qq.com>
2020-02-21 11:44:43 +0800

    文章详情-关注功能-创建api函数

--- src/api/user.js
+++ src/api/user.js
@@ -2,6 +2,31 @@
 import request from '@/utils/request.js'
 
 // 创建各种api方法
+/**
+ * 关注作者
+ * @param {target} 被关注用户id
+ */
+export function apiFollow (target) {
+  return request({
+    method: 'post',
+    url: '/app/v1_0/user/followings',
+    data: {
+      // 成员简易赋值
+      target
+    }
+  })
+}
+
+/**
+ * 取消关注作者
+ * @param {target} 取消关注用户id
+ */
+export function apiUnFollow (target) {
+  return request({
+    method: 'delete',
+    url: `/app/v1_0/user/followings/${target}`
+  })
+}
 
 /**
  * 对登录账户信息进行校验，是一个es6按需导出动作
